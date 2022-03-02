4b7e2f9b39a77e4b1238285feab1140153e96a65
e97m <majdalawi97@yahoo.com>
2022-03-02 14:39:07 +0300

    add Lucas

--- math_series/series.py
+++ math_series/series.py
@@ -18,3 +18,51 @@ def fibonacci(n):
         return (f'Fibonacci sequence upto the position {n} is: {series}')
 
 
+
+
+# def calculate_lucas(n):
+#     if n == 1:
+#         return 2
+#     elif n == 2:
+#         return 1
+#     else:
+#        return(calculate_lucas(n-1) + calculate_lucas(n-2))
+
+# def lucas(n):
+#     series =[]
+#     if type(n) != int:
+#         return ("Please enter a positive integer")
+#     elif n <= 0:
+#         return ("Please enter a positive integer")
+#     else:
+#         for i in range(n):
+#             series.append(calculate_lucas(i))
+#         return (f'Lucas sequence upto the position {n} is: {series}')
+
+
+def lucas(n) :
+    if type(n) != int:
+        return ("Please enter a positive integer")
+    elif n <= 0:
+        return ("Please enter a positive integer")
+    else:
+
+        series = []
+        a = 2
+        b = 1
+        
+        if (n == 1) :
+            series = [a]
+            return (f'Lucas sequence upto the position {n} is: {series}')
+        elif n == 2:
+            series = [a, b]
+            return (f'Lucas sequence upto the position {n} is: {series}')
+        else:
+            series.append(2)
+            series.append(1)
+            for i in range(2, n) :
+                c = a + b
+                a = b
+                b = c
+                series.append(b)
+        return (f'Lucas sequence upto the position {n} is: {series}')
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 def test_fibonacci_0():
     actual = fibonacci(0)
@@ -37,5 +37,48 @@ def test_fibonacci_float():
 
 def test_fibonacci_a():
     actual = fibonacci('a')
+    expected = 'Please enter a positive integer'
+    assert(actual == expected)
+
+
+
+
+def test_lucas_0():
+    actual = lucas(0)
+    expected = 'Please enter a positive integer'
+    assert(actual == expected)
+
+def test_lucas_1():
+    actual = lucas(1)
+    expected = 'Lucas sequence upto the position 1 is: [2]'
+    assert(actual == expected)
+
+def test_lucas_2():
+    actual = lucas(2)
+    expected = 'Lucas sequence upto the position 2 is: [2, 1]'
+    assert(actual == expected)
+
+def test_lucas_3():
+    actual = lucas(3)
+    expected = 'Lucas sequence upto the position 3 is: [2, 1, 3]'
+    assert(actual == expected)
+
+def test_lucas_10():
+    actual = lucas(10)
+    expected = 'Lucas sequence upto the position 10 is: [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]'
+    assert(actual == expected)
+
+def test_lucas_neg3():
+    actual = lucas(-3)
+    expected = 'Please enter a positive integer'
+    assert(actual == expected)
+
+def test_lucas_float():
+    actual = lucas(3.3)
+    expected = 'Please enter a positive integer'
+    assert(actual == expected)
+
+def test_lucas_a():
+    actual = lucas('a')
     expected = 'Please enter a positive integer'
     assert(actual == expected)
